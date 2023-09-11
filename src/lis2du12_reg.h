8dab550b8d89cad33fd4aaeedbcf20c52166c5b2
Carlo Parata <carlo.parata@st.com>
2023-09-11 14:42:02 +0200

    Update lis2du12_reg.h
    Signed-off-by: Carlo Parata <carlo.parata@st.com>

--- src/lis2du12_reg.h
+++ src/lis2du12_reg.h
@@ -101,35 +101,6 @@ typedef struct {
 
 #endif /* MEMS_SHARED_TYPES */
 
-/**
-  * @}
-  *
-  */
-
-/** @addtogroup  Interfaces_Functions
-  * @brief       This section provide a set of functions used to read and
-  *              write a generic register of the device.
-  *              MANDATORY: return 0 -> no Error.
-  * @{
-  *
-  */
-
-typedef int32_t (*lis2du12_write_ptr)(void *, uint8_t, uint8_t *, uint16_t);
-typedef int32_t (*lis2du12_read_ptr)(void *, uint8_t, uint8_t *, uint16_t);
-
-typedef struct {
-  /** Component mandatory fields **/
-  lis2du12_write_ptr  write_reg;
-  lis2du12_read_ptr   read_reg;
-  /** Customizable optional pointer **/
-  void *handle;
-} lis2du12_ctx_t;
-
-/**
-  * @}
-  *
-  */
-
 #ifndef MEMS_UCF_SHARED_TYPES
 #define MEMS_UCF_SHARED_TYPES
 
@@ -155,6 +126,35 @@ typedef struct {
   */
 
 #endif /* MEMS_UCF_SHARED_TYPES */
+
+/**
+  * @}
+  *
+  */
+
+/** @addtogroup  LIS2DU12_Interfaces_Functions
+  * @brief       This section provide a set of functions used to read and
+  *              write a generic register of the device.
+  *              MANDATORY: return 0 -> no Error.
+  * @{
+  *
+  */
+
+typedef int32_t (*lis2du12_write_ptr)(void *, uint8_t, uint8_t *, uint16_t);
+typedef int32_t (*lis2du12_read_ptr)(void *, uint8_t, uint8_t *, uint16_t);
+
+typedef struct {
+  /** Component mandatory fields **/
+  lis2du12_write_ptr  write_reg;
+  lis2du12_read_ptr   read_reg;
+  /** Customizable optional pointer **/
+  void *handle;
+} lis2du12_ctx_t;
+
+/**
+  * @}
+  *
+  */
 
 /** @defgroup LIS2DU12_Infos
   * @{
